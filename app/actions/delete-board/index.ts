0c4e3022432b7c4b9c864b29622ae7afd91429de
unknown <dumbirichuks@gmail.com>
2024-02-12 17:55:34 +0100

    updated the delete-board

--- app/actions/delete-board/index.ts
+++ app/actions/delete-board/index.ts
@@ -11,6 +11,7 @@ import { redirect } from "next/navigation";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { decreaseAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 
 const handler = async (data: InputType): Promise<ReturnType> => {
@@ -23,6 +24,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         }
     }
 
+    const isPro = await checkSubscription();
+
+
     const { id } = data;
     let board;
 
@@ -34,7 +38,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
             },
         })
 
-        await decreaseAvailableCount();
+        if (!isPro) {
+            await decreaseAvailableCount();
+        }
 
         await createAuditLog({
             entityTitle: board.title,
